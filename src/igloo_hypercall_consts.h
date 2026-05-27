0547f0acb5fe8001344105ce2198ae6f77df5976
Luke Craig <lacraig3@gmail.com>
2026-05-26 22:43:30 -0400

    Drop unused task switch hypercall constant

--- src/igloo_hypercall_consts.h
+++ src/igloo_hypercall_consts.h
@@ -31,7 +31,6 @@ enum igloo_hypercall_constants {
     IGLOO_HYP_SYSCALL_RETURN = 0x1339,
     IGLOO_HYP_SETUP_TASK_COMM = 0x133a,
     IGLOO_HYP_SIGNAL_DELIVER = 0x133b,
-    IGLOO_HYP_OSI_TASK_SWITCH = 0x3337,
     
     /* Uprobe operations */
     IGLOO_HYP_UPROBE_ENTER  = 0x6901,
